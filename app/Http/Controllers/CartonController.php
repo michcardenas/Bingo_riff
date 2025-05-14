a71ff414da926dd78f036cbefe820a4ba78bbd37
michael.cardenas <michael.cardenas@realtechltda.com>
2025-05-14 18:36:26 -0500

    ss

--- app/Http/Controllers/CartonController.php
+++ app/Http/Controllers/CartonController.php
@@ -389,7 +389,7 @@ public function descargar($numero, $bingoId = null) {
                 $fontSize = 16;
                 
                 // Calcular posiciones para alinear a la derecha (con un margen de 20px)
-                $margenDerecho = 40;
+                $margenDerecho = 80;
                 
                 // Posición X para alinear a la derecha
                 $bbox1 = imagettfbbox($fontSize, 0, $fuente, $textoBingo);
