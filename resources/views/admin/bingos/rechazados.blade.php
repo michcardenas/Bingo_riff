105445ebf9d4bcd3fd950cb6b8711884b550a911
michael.cardenas <michael.cardenas@realtechltda.com>
2025-05-15 16:53:46 -0500

    ss

--- resources/views/admin/bingos/rechazados.blade.php
+++ resources/views/admin/bingos/rechazados.blade.php
@@ -37,180 +37,56 @@
                         </h5>
                     </div>
                     <div class="card-body">
-                        <!-- Pestañas para navegar entre las dos secciones -->
-                        <ul class="nav nav-tabs" id="rechazadosTabs" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link active" id="reservas-tab" data-bs-toggle="tab" 
-                                        data-bs-target="#reservas" type="button" role="tab" 
-                                        aria-controls="reservas" aria-selected="true">
-                                    Reservas Rechazadas
-                                </button>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link" id="cartones-tab" data-bs-toggle="tab" 
-                                        data-bs-target="#cartones" type="button" role="tab" 
-                                        aria-controls="cartones" aria-selected="false">
-                                    Cartones Individuales
-                                </button>
-                            </li>
-                        </ul>
-                        
-                        <!-- Contenido de las pestañas -->
-                        <div class="tab-content mt-3" id="rechazadosTabsContent">
-                            <!-- Pestaña 1: Reservas Rechazadas -->
-                            <div class="tab-pane fade show active" id="reservas" role="tabpanel" aria-labelledby="reservas-tab">
-                                @if(count($reservasRechazadas) > 0)
-                                    <div class="table-responsive">
-                                        <table class="table table-striped table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>ID</th>
-                                                    <th>Nombre</th>
-                                                    <th>Celular</th>
-                                                    <th>Cantidad</th>
-                                                    <th>Total</th>
-                                                    <th>Cartones</th>
-                                                    <th>Fecha</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach($reservasRechazadas as $item)
-                                                    <tr>
-                                                        <td>{{ $item['reserva']->id }}</td>
-                                                        <td>{{ $item['reserva']->nombre }}</td>
-                                                        <td>{{ $item['reserva']->celular }}</td>
-                                                        <td>{{ $item['reserva']->cantidad }}</td>
-                                                        <td>${{ number_format($item['reserva']->total, 0, ',', '.') }}</td>
-                                                        <td>
-                                                            <button type="button" class="btn btn-sm btn-info" 
-                                                                    data-bs-toggle="modal" 
-                                                                    data-bs-target="#cartonesModal{{ $item['reserva']->id }}">
-                                                                Ver {{ count($item['cartones']) }} cartones
-                                                            </button>
-                                                        </td>
-                                                        <td>{{ date('d/m/Y H:i', strtotime($item['reserva']->created_at)) }}</td>
-                                                    </tr>
-                                                    
-                                                    <!-- Modal para ver los cartones de esta reserva -->
-                                                    <div class="modal fade" id="cartonesModal{{ $item['reserva']->id }}" tabindex="-1" 
-                                                         aria-labelledby="cartonesModalLabel{{ $item['reserva']->id }}" aria-hidden="true">
-                                                        <div class="modal-dialog modal-lg">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <h5 class="modal-title" id="cartonesModalLabel{{ $item['reserva']->id }}">
-                                                                        Cartones de la Reserva #{{ $item['reserva']->id }}
-                                                                    </h5>
-                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                                </div>
-                                                                <div class="modal-body">
-                                                                    <div class="table-responsive">
-                                                                        <table class="table table-sm">
-                                                                            <thead>
-                                                                                <tr>
-                                                                                    <th>Cartón</th>
-                                                                                    <th>Series</th>
-                                                                                </tr>
-                                                                            </thead>
-                                                                            <tbody>
-                                                                                @foreach($item['cartones'] as $carton)
-                                                                                    <tr>
-                                                                                        <td>{{ $carton }}</td>
-                                                                                        <td>
-                                                                                            @if(isset($item['cartonesSeries'][$carton]))
-                                                                                                @if(is_array($item['cartonesSeries'][$carton]))
-                                                                                                    {{ implode(', ', $item['cartonesSeries'][$carton]) }}
-                                                                                                @else
-                                                                                                    {{ $item['cartonesSeries'][$carton] }}
-                                                                                                @endif
-                                                                                            @else
-                                                                                                No disponible
-                                                                                            @endif
-                                                                                        </td>
-                                                                                    </tr>
-                                                                                @endforeach
-                                                                            </tbody>
-                                                                        </table>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                @else
-                                    <div class="alert alert-info">
-                                        No hay reservas rechazadas para este bingo.
-                                    </div>
-                                @endif
-                            </div>
-                            
-                            <!-- Pestaña 2: Cartones Individuales -->
-                            <div class="tab-pane fade" id="cartones" role="tabpanel" aria-labelledby="cartones-tab">
-                                @if(count($cartonesRechazados) > 0)
-                                    <div class="table-responsive">
-                                        <table class="table table-striped table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>ID</th>
-                                                    <th>Cartón</th>
-                                                    <th>Reserva ID</th>
-                                                    <th>Nombre</th>
-                                                    <th>Series</th>
-                                                    <th>Fecha Rechazo</th>
-                                                    <th>Motivo</th>
-                                                    <th>Usuario</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach($cartonesRechazados as $item)
-                                                    <tr>
-                                                        <td>{{ $item['carton']->id }}</td>
-                                                        <td>{{ $item['carton']->serie_rechazada }}</td>
-                                                        <td>
-                                                            @if($item['reserva'])
-                                                                {{ $item['reserva']->id }}
-                                                            @else
-                                                                N/A
-                                                            @endif
-                                                        </td>
-                                                        <td>
-                                                            @if($item['reserva'])
-                                                                {{ $item['reserva']->nombre }}
-                                                            @else
-                                                                N/A
-                                                            @endif
-                                                        </td>
-                                                        <td>
-                                                            @if(isset($item['series']))
-                                                                @if(is_array($item['series']))
-                                                                    {{ implode(', ', $item['series']) }}
-                                                                @else
-                                                                    {{ $item['series'] }}
-                                                                @endif
-                                                            @else
-                                                                No disponible
-                                                            @endif
-                                                        </td>
-                                                        <td>{{ date('d/m/Y H:i', strtotime($item['carton']->fecha_rechazo)) }}</td>
-                                                        <td>{{ $item['carton']->motivo ?? 'No especificado' }}</td>
-                                                        <td>{{ $item['carton']->usuario ?? 'Sistema' }}</td>
-                                                    </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                @else
-                                    <div class="alert alert-info">
-                                        No hay cartones individuales rechazados para este bingo.
-                                    </div>
-                                @endif
-                            </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Celular</th>
+                                        <th>Cartón</th>
+                                        <th>Series</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($cartonesRechazados as $item)
+                                        <tr>
+                                            <td>
+                                                @if($item['reserva'])
+                                                    {{ $item['reserva']->nombre }}
+                                                @else
+                                                    N/A
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if($item['reserva'])
+                                                    {{ $item['reserva']->celular }}
+                                                @else
+                                                    N/A
+                                                @endif
+                                            </td>
+                                            <td>{{ $item['carton']->serie_rechazada }}</td>
+                                            <td>
+                                                @if(isset($item['series']))
+                                                    @if(is_array($item['series']))
+                                                        {{ implode(', ', $item['series']) }}
+                                                    @else
+                                                        {{ $item['series'] }}
+                                                    @endif
+                                                @else
+                                                    No disponible
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
+                        
+                        @if(count($cartonesRechazados) == 0)
+                            <div class="alert alert-info">
+                                No hay cartones rechazados para este bingo.
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
