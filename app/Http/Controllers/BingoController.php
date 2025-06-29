c641f9990afc6de585965ed3f4869014dcbd2bc9
michael.cardenas <michael.cardenas@realtechltda.com>
2025-06-29 15:21:32 -0500

    ss

--- app/Http/Controllers/BingoController.php
+++ app/Http/Controllers/BingoController.php
@@ -1824,7 +1824,7 @@ public function descargarCarton($bingoId, $serie)
                 $fontSize = 16;
                 
                 // Márgenes y posiciones
-                $margenDerecho = 500;
+                $margenDerecho = 900;
                 $margenIzquierdo = 20;
                 
                 // Calcular el ancho máximo disponible para el texto
